f71812b3dd63052a91ae7a3441261e852d95d058
Arya Dwi Putra <aryadptr.business@gmail.com>
2023-08-27 17:39:40 +0700

    Upload assets for dashboard template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,21 +1,46 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+<head>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <title>Blog</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/images/logo/favicon.svg') }}">
+    <!-- BEGIN: Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
+    <!-- END: Google Font -->
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" href="{{ assset('assets/css/sidebar-menu.css') }}">
+    <link rel="stylesheet" href="{{ assset('assets/css/SimpleBar.css') }}">
+    <link rel="stylesheet" href="{{ assset('assets/css/app.css') }}">
+    <!-- END: Theme CSS-->
+    <script src="{{ asst('assets/js/settings.js') }}" sync></script>
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
+
+<body class="font-sans antialiased">
+    @inertia
+
+
+    <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.js') }}"></script>
+    <script src="{{ asset('assets/js/tw-elements-1.0.0-alpha13.min.js') }}"></script>
+    <script src="{{ asset('assets/js/SimpleBar.js') }}"></script>
+    <script src="{{ asset('assets/js/iconify.js') }}"></script>
+    <!-- Jquery Plugins -->
+
+    <!-- app js -->
+    <script src="{{ asset('assets/js/sidebar-menu.js') }}"></script>
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+</body>
+
 </html>
